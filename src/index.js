510df852508d39dfa26c3f90d00fc6208638be45
Viktor <shevchukviktor42@gmail.com>
2020-04-20 14:26:16 +0300

    Христос Воскрес

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyInfo from "./components/MyInfo";
+
+
+ReactDOM.render(
+ <MyInfo />,
+  document.getElementById('root')
+);
+
+
